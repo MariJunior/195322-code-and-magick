4b1b446d7337525a72d3dfeb597cfbeeca4215d7
MariJunior <marinakaluzhnaya13@gmail.com>
2018-07-14 00:38:31 +0500

    Загружает данные похожих персонажей других игроков с сервера; отправляет данные игрока на сервер.

--- js/setup.js
+++ js/setup.js
@@ -1,26 +1,6 @@
 'use strict';
 
 (function () {
-  var FIRST_NAMES = [
-    'Иван',
-    'Хуан Себастьян',
-    'Мария',
-    'Кристоф',
-    'Виктор',
-    'Юлия',
-    'Люпита',
-    'Вашингтон'
-  ];
-  var LAST_NAMES = [
-    'да Марья',
-    'Верон',
-    'Мирабелла',
-    'Вальц',
-    'Онопко',
-    'Топольницкая',
-    'Нионго',
-    'Ирвинг'
-  ];
   var COAT_COLORS = [
     'rgb(101, 137, 164)',
     'rgb(241, 43, 107)',
@@ -44,7 +24,6 @@
     '#e6e848'
   ];
   var WIZARDS_QUANTITY = 4;
-  var wizards = [];
 
   var userDialog = document.querySelector('.setup');
   var wizardsInterface = document.querySelector('.setup-similar');
@@ -57,19 +36,10 @@
   var coatColorInput = userDialog.querySelector('input[name = coat-color]');
   var eyeColorInput = userDialog.querySelector('input[name = eyes-color]');
   var fireballColorInput = userFireball.querySelector('input[name = fireball-color]');
+  var userForm = userDialog.querySelector('.setup-wizard-form');
 
   var getRandomElement = function (arr) {
     return arr[Math.floor(Math.random() * (arr.length))];
-  };
-
-  var getRandomWizard = function () {
-    var wizard = {
-      name: getRandomElement(FIRST_NAMES) + ' ' + getRandomElement(LAST_NAMES),
-      coatColor: getRandomElement(COAT_COLORS),
-      eyesColor: getRandomElement(EYES_COLORS)
-    };
-
-    return wizard;
   };
 
   var createWizard = function (object) {
@@ -79,15 +49,15 @@
     var wizardEyes = wizardTemplate.querySelector('.wizard-eyes');
 
     wizardName.textContent = object.name;
-    wizardCoat.style.fill = object.coatColor;
-    wizardEyes.style.fill = object.eyesColor;
+    wizardCoat.style.fill = object.colorCoat;
+    wizardEyes.style.fill = object.colorEyes;
 
     return wizardTemplate;
   };
 
   var renderWizards = function (wizardsArr) {
-    for (i = 0; i < wizardsArr.length; i++) {
-      var newWizard = createWizard(wizardsArr[i]);
+    for (var i = 0; i < WIZARDS_QUANTITY; i++) {
+      var newWizard = createWizard(getRandomElement(wizardsArr));
 
       fragment.appendChild(newWizard);
     }
@@ -102,11 +72,33 @@
     input.value = color;
   };
 
-  for (var i = 0; i < WIZARDS_QUANTITY; ++i) {
-    wizards[i] = getRandomWizard();
-  }
-  renderWizards(wizards);
-  wizardsInterface.classList.remove('hidden');
+  var errorHandler = function (errorMessage) {
+    var node = document.createElement('div');
+    node.style = 'z-index: 100; margin: 0 auto; text-align: center; background-color: tomato;';
+    node.style.position = 'absolute';
+    node.style.left = 0;
+    node.style.right = 0;
+    node.style.fontSize = '30px';
+    node.textContent = errorMessage;
+    document.body.insertAdjacentElement('afterbegin', node);
+    setTimeout(function () {
+      node.remove();
+    }, 3000);
+  };
+
+  var successHandler = function () {
+    userDialog.classList.add('hidden');
+    userForm.reset();
+  };
+
+  window.backend.load(
+      function (wizards) {
+        renderWizards(wizards);
+
+        wizardsInterface.classList.remove('hidden');
+      },
+      errorHandler
+  );
 
   userWizardCoat.addEventListener('click', function () {
     getColor(userWizardCoat, COAT_COLORS, 'fill', coatColorInput);
@@ -118,5 +110,10 @@
 
   userFireball.addEventListener('click', function () {
     getColor(userFireball, FIREBALL_COLORS, 'background', fireballColorInput);
+  });
+
+  userForm.addEventListener('submit', function (evt) {
+    evt.preventDefault();
+    window.backend.save(new FormData(userForm), successHandler, errorHandler);
   });
 })();
